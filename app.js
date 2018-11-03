04a2cc8e4164541be8d4e2dd83e287e175c0a555
Geraldine13 <geral1312@hotmail.com>
2018-11-03 02:33:03 -0500

    Corrigiendo portafolio

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems);
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left',
+    hoverEnabled: false
+  });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
